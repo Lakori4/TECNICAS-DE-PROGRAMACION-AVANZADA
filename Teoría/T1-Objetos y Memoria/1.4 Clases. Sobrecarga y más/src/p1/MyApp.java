01ffbf1b8f8fd0538a336215fea9ab963041910c
Santiago Valderrama <santivalde04@gmail.com>
2023-09-18 19:18:41 +0200

    main fill

--- Teoría/T1-Objetos y Memoria/1.4 Clases. Sobrecarga y más/src/p1/MyApp.java
+++ Teoría/T1-Objetos y Memoria/1.4 Clases. Sobrecarga y más/src/p1/MyApp.java
@@ -8,5 +8,43 @@ public class MyApp {
         
         double [] valores = {4, 5, 8, 9};
         ArrayAverage A3 = new ArrayAverage(valores);
+
+        System.out.println(A1.getMedia());
+        System.out.println(A2.getMedia());
+        System.out.println(A3.getMedia());
+
+        Semana hoy = Semana.LUNES;
+        switch (hoy) {
+            case LUNES:
+                System.out.println("Hoy es lunes.");
+                break;
+
+            case MARTES:
+                System.out.println("Hoy es martes.");
+                break;
+
+            case MIÉRCOLES:
+                System.out.println("Hoy es miércoles.");
+                break;
+
+            case JUEVES:
+                System.out.println("Hoy es jueves.");
+                break;
+
+            case VIERNES:
+                System.out.println("Hoy es viernes.");
+                break;
+
+            case SÁBADO:
+                System.out.println("Hoy es sábado.");
+                break;
+
+            case DOMINGO:
+                System.out.println("Hoy es domingo.");
+                break;
+        
+            default:
+                break;
+        }
     }
 }
